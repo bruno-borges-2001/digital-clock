6b6f3677047c6ab7695eff9a1f6a3e497f4f50e7
Bruno Borges <brunoborges2001@gmail.com>
2022-12-30 16:08:20 -0300

    added background to body

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Clock from "./components/Clock"
 function App() {
 
   return (
-    <div className="h-[100vh] flex justify-center items-center bg-gray-400">
+    <div className="h-[100vh] flex justify-center items-center">
       <Clock />
     </div>
   )
